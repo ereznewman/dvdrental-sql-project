9cb35343067a80e6f3e2024d2a1c37fb88574150
Erez Newman <ereznewman@gmail.com>
2025-07-27 12:06:10 +0100

    Create 04_revenue_and_staff_trends.sql

---
+++ 04_revenue_and_staff_trends.sql
@@ -0,0 +1,36 @@
+--What is the total revenue generated per month?
+ 
+SELECT 
+EXTRACT(month FROM payment_date) AS Month,
+EXTRACT(year FROM payment_date) AS Year,
+SUM(amount) AS total_revenue FROM payment 
+GROUP BY Month,Year
+ORDER BY total_revenue DESC;
+
+
+--Which staff member processed the most payments each month?
+
+SELECT Year, Month, staff_id, first_name, last_name, total_revenue
+FROM (
+  SELECT 
+    EXTRACT(year FROM payment_date) AS Year,
+    EXTRACT(month FROM payment_date) AS Month,
+    staff.staff_id,
+    staff.first_name,
+    staff.last_name,
+    SUM(amount) AS total_revenue,
+    ROW_NUMBER() OVER (
+      PARTITION BY EXTRACT(year FROM payment_date), EXTRACT(month FROM payment_date)
+      ORDER BY SUM(amount) DESC
+    ) AS rn
+  FROM payment
+  JOIN staff ON payment.staff_id = staff.staff_id
+  GROUP BY 
+    EXTRACT(year FROM payment_date), 
+    EXTRACT(month FROM payment_date), 
+    staff.staff_id, 
+    staff.first_name, 
+    staff.last_name
+) AS sub
+WHERE rn = 1
+ORDER BY Year, Month;
